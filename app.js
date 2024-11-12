568e28b8a953b90f4296a08f6af508d2ca3e7066
Mussorable <redduck5601@gmail.com>
2024-11-12 22:15:53 +0100

    mysql init, added connection to db, dotenv

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
+
 const http = require('http');
-
 const express = require('express');
-
 const app = express();
-const server = http.createServer(app);
+const db = require('./util/database');
+const bodyParser = require('body-parser');
 
+db.execute('SELECT * FROM users')
+  .then(result => {
+    console.log(result[0]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const server = http.createServer(app);
 server.listen(3000);
