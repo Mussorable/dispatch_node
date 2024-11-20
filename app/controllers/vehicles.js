a602bc4070868fde15bf2af4c2f8d76e5e2d7932
Mussorable <redduck5601@gmail.com>
2024-11-20 20:24:49 +0100

    Refactor vehicle and driver controllers.
    Refactor the vehicle and driver controller methods. Introduced `getAllRecords` and `createRecord` functions in `modelOperations.js`.

--- app/controllers/vehicles.js
+++ app/controllers/vehicles.js
@@ -1,56 +1,24 @@
 const Truck = require('../models/truck');
 const Trailer = require('../models/trailer');
-const Driver = require("../models/driver");
+const {getAllRecords, createRecord} = require("../util/modelOperations");
 
 exports.getVehicles = async (req, res) => {
     const type = req.params.type;
     if (type === 'trucks') {
-        const trucks = await Truck.findAll();
-        res.status(200).json({
-            message: 'Trucks retrieved successfully',
-            trucks: trucks.map(truck => truck.get({plain: true}))
-        });
+        await getAllRecords(Truck, req, res, 'Trucks retrieved successfully');
     } else if (type === 'trailers') {
-        const trailers = await Trailer.findAll();
-        res.status(200).json({
-            message: 'Trailers retrieved successfully',
-            trailers: trailers.map(trailer => trailer.get({plain: true}))
-        });
+        await getAllRecords(Trailer, req, res, 'Trailers retrieved successfully');
     }
 };
 
 exports.addVehicle = async (req, res) => {
     const type = req.params.type;
-    const {number, driverNumber, additionalNumber} = req.body;
 
     if (type === 'truck') {
-        const existingTruck = await Truck.findOne({where: {number: number}});
-        if (existingTruck) {
-            return res.status(400).json({message: 'Truck already exists'});
-        }
-
-        const truck = await Truck.create({
-            number: number,
-        });
-
-        res.status(201).json({
-            message: 'Truck created successfully',
-            truck: truck.get({plain: true})
-        });
+        await createRecord(Truck, req, res, ['number'], 'Truck added successfully');
     } else if (type === 'trailer') {
-        const existingTrailer = await Trailer.findOne({where: {number: number}});
-        if (existingTrailer) {
-            return res.status(400).json({message: 'Trailer already exists'});
-        }
-
-        const trailer = await Trailer.create({
-            number: number,
-        });
-
-        res.status(201).json({
-            message: 'Trailer created successfully',
-            trailer: trailer.get({plain: true})
-        });
+        const response = await createRecord(Trailer, req, res, ['number'], 'Trailer added successfully');
+        console.log(response);
     }
 };
 
